94cbb5f9c271b8217bad5901911b3586ebd1b884
Matheus Andrade <matheusgandrade@poli.ufrj.br>
2018-08-21 20:07:43 -0300

    expand docs with reference and add command line arguments

--- ring_quantum_walk.py
+++ ring_quantum_walk.py
@@ -1,3 +1,5 @@
+import sys
+
 import projectq.setups.default
 
 from projectq import MainEngine
@@ -7,9 +9,19 @@ from projectq.types import WeakQubitRef, Qureg
 
 from numpy import base_repr
 
+"""
+This script implements quantum walks on generic cycles following the circuit
+structure defined in 'https://arxiv.org/pdf/0706.0304.pdf' ([1]). It works as a
+coinned quantum walk using a single coin qbit and increments and decrements of
+the vertex state basis. The coin is represented by a Hadamard gate applied over
+a prepared qubit on state |0>.
+"""
+
 # Performs a controlled gate mixing off controls, active when qubit is 0, and
 #  normal controls. active when qubit is 1. Sel is a binary array referencing
 #  which qubits will be off and which will be normal.
+
+
 def generic_control(sel, controls, targets, gate, eng, nodes):
 	for j in range(len(sel)): # prepare controls evaluated on qubits
 		if not sel[j]:
@@ -24,8 +36,8 @@ def generic_control(sel, controls, targets, gate, eng, nodes):
 			X | nodes[controls[j]]
 
 
-
-# Performs a increment(inc = 1) or decrement (inc = 0) counter on states given inc parameter.
+# Performs a increment(inc = 1) or decrement (inc = 0) counter on states given 
+#  inc parameter.
 def counter(eng, nodes, n_qbits, inc=1):
 	control_bit = n_qbits - 1
 
@@ -34,15 +46,21 @@ def counter(eng, nodes, n_qbits, inc=1):
 		generic_control(sel, list(range(i + 1, n_qbits)), [i], X, eng, nodes)
 
 
-
 # Bound counters for cases when graph size (number of vertices) isn't a power 
-#  of 2.
+#  of 2. When incrementing, the state |bin_gsize> must be mapped to the state 
+#  |0>.
 def cyclic_increment_bound(eng, nodes, n_qbits, bin_gsize):
 	sel = bin_gsize + [1]
 	n_of_ones = sum(sel)
-	line = 0
-	ind = 0
+
+	line = 0 # index for qubits representing vertex label
+	ind = 0 # counter of qbits on state 1
+
+	# auxialiary list to define index of selection qbits for each iteration
 	qubits = list(range(n_qbits))
+
+	# list to accumulate parameters used by generic_control to map |bin_gsize>
+	#  to |0>
 	params = list()
 	while ind < n_of_ones / 2:
 		if sel[line]:
@@ -52,18 +70,28 @@ def cyclic_increment_bound(eng, nodes, n_qbits, bin_gsize):
 			generic_control(*params[-1])
 			ind += 1
 		line += 1
+
+	# When incrementing, it's necessary to fix flipped bits of states that 
+	#  activate selectors of previous sections.
 	for p in params[-2::-1]:
 		generic_control(*p)
 
 
-
-
+# Bound counters for cases when graph size (number of vertices) isn't a power 
+#  of 2. When decrementing a vertex label, it's necessary to map the 0 state to
+#  the state expressed by bin_gsize.
 def cyclic_decrement_bound(eng, nodes, n_qbits, bin_gsize):
 	sel = bin_gsize + [0]
 	n_of_zeros = n_qbits - sum(sel)
-	line = n_qbits - 2
-	ind = 0
+
+	line = n_qbits - 2 # index for qubits representing vertex label
+	ind = 0 # counter of qbits on state 0
+
+	# auxialiary list to define index of selection qbits for each iteration
 	qubits = list(range(n_qbits))
+
+	# auxiliary list to gradually define control types for node qubits when
+	#  mapping state |0> to |bin_gsize>
 	mask = [1] * (n_qbits - 1)
 	mask.append(0)
 
@@ -91,23 +119,36 @@ def walk(eng, nodes, n_qbits, steps, graph_size):
 	H | nodes[n_qbits - 1] # uses last bit as coin for cycle
 
 	for s in range(steps):
+
 		counter(eng, nodes, n_qbits, inc=1) # increment
 
 		# bound increment iff graph size isn't a power of 2
+		#  see Figure 3 of [1] for more clarification
 		bounds[0](eng, nodes, n_qbits, bin_gsize)
 
 		counter(eng, nodes, n_qbits, inc=0) # decrement
 
-		# bound increment iff graph size isn't a power of 2
+		# bound decrement iff graph size isn't a power of 2
+		#  see Figure 3 of [1] for more clarification
 		bounds[1](eng, nodes, n_qbits, bin_gsize)
 
 
-eng = MainEngine ()
+# standard parameters for script, 10-step walk on a 32-vertex graph
 n_qbits = 6
 graph_size = 32
-nodes = eng.allocate_qureg(n_qbits)
 steps = 10
-walk(eng, nodes, n_qbits, steps, graph_size)
-Measure | (nodes)
-for node in nodes:
-	print(int(node))
+
+if len(sys.argv) == 4:
+	n_qbits = int(sys.argv[1]) 
+	graph_size = int(sys.argv[2])
+	steps = int(sys.argv[3])
+
+if (2 ** (n_qbits - 1) < graph_size):
+	print("Not enough qbits to represent given number of vertices!")
+else:
+	eng = MainEngine()
+	nodes = eng.allocate_qureg(n_qbits)
+	walk(eng, nodes, n_qbits, steps, graph_size)
+	Measure | (nodes)
+	for node in nodes:
+		print(int(node))
