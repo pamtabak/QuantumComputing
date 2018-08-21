6b79ff7f47a8ea76c2419fdc5a26b17b302a0125
Matheus Andrade <matheusgandrade@poli.ufrj.br>
2018-08-21 19:35:00 -0300

    make it work for cycles with size different than power of 2

--- ring_quantum_walk.py
+++ ring_quantum_walk.py
@@ -5,7 +5,7 @@ from projectq.ops import H, Measure, CNOT, X
 from projectq.meta import Dagger, Control
 from projectq.types import WeakQubitRef, Qureg
 
-from numpy import basis_repr
+from numpy import base_repr
 
 # Performs a controlled gate mixing off controls, active when qubit is 0, and
 #  normal controls. active when qubit is 1. Sel is a binary array referencing
@@ -37,12 +37,12 @@ def counter(eng, nodes, n_qbits, inc=1):
 
 # Bound counters for cases when graph size (number of vertices) isn't a power 
 #  of 2.
-def cyclic_bound(eng, nodes, n_qbits, graph_size):
-	sel = [int(i) for i in basis_repr(graph_size)] + [1]
+def cyclic_increment_bound(eng, nodes, n_qbits, bin_gsize):
+	sel = bin_gsize + [1]
 	n_of_ones = sum(sel)
 	line = 0
 	ind = 0
-	qubits = range(n_qbits)
+	qubits = list(range(n_qbits))
 	params = list()
 	while ind < n_of_ones / 2:
 		if sel[line]:
@@ -57,38 +57,57 @@ def cyclic_bound(eng, nodes, n_qbits, graph_size):
 
 
 
-def cyclic_decrement_bound(eng, nodes, n_qbits, graph_size):
-	sel = [int(i) for i in basis_repr(graph_size)] + [1]
-	n_of_ones = sum(sel)
-	line = 0
+
+def cyclic_decrement_bound(eng, nodes, n_qbits, bin_gsize):
+	sel = bin_gsize + [0]
+	n_of_zeros = n_qbits - sum(sel)
+	line = n_qbits - 2
 	ind = 0
-	qubits = range(n_qbits)
-	params = list()
-	while ind < n_of_ones / 2:
+	qubits = list(range(n_qbits))
+	mask = [1] * (n_qbits - 1)
+	mask.append(0)
+
+	while ind < n_of_zeros:
 		if not sel[line]:
-			params.append(
-				(sel[:line] + sel[line + 1:], qubits[:line] + qubits[line +1 :],
-				[line], X, eng, nodes))
-			generic_control(*params[-1])
+			generic_control(
+				mask[:line] + mask[line + 1:],
+				qubits[:line] + qubits[line +1 :],
+				[line], X, eng, nodes)
+			mask[line] = 0
 			ind += 1
-		line += 1
-	for p in params[-2::-1]:
-		generic_control(*p)
+		line -= 1
 
 
 # Perform a given number of steps of a cycle
-def walk(eng, nodes, n_qbits, steps):
+def walk(eng, nodes, n_qbits, steps, graph_size):
+
+	bounds = (lambda eng, nodes, n_qbits, bin_gsize: 0,
+			  lambda eng, nodes, n_qbits, bin_gsize: 0)
+	bin_gsize = [int(i) for i in base_repr(graph_size - 1)]
+
+	if sum(bin_gsize) != (n_qbits - 1): # allow bounds iff not power of 2
+		bounds = (cyclic_increment_bound, cyclic_decrement_bound)
+
 	H | nodes[n_qbits - 1] # uses last bit as coin for cycle
+
 	for s in range(steps):
 		counter(eng, nodes, n_qbits, inc=1) # increment
+
+		# bound increment iff graph size isn't a power of 2
+		bounds[0](eng, nodes, n_qbits, bin_gsize)
+
 		counter(eng, nodes, n_qbits, inc=0) # decrement
+
+		# bound increment iff graph size isn't a power of 2
+		bounds[1](eng, nodes, n_qbits, bin_gsize)
 
 
 eng = MainEngine ()
 n_qbits = 6
+graph_size = 32
 nodes = eng.allocate_qureg(n_qbits)
-steps = 1
-walk(eng, nodes, n_qbits, steps)
+steps = 10
+walk(eng, nodes, n_qbits, steps, graph_size)
 Measure | (nodes)
 for node in nodes:
 	print(int(node))
